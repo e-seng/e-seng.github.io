20c287f4305038ffd57ac7596851c97a05d07abf
e-seng <eth.sengsavang@gmail.com>
2021-01-22 14:55:43 -0700

    Add code block with document html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 function init(){
     console.log("hello")
+    
+    function main(){
+        populateSquare();
+    }
+
+    function populateSquare(){
+        let pageHtml = document.querySelector("html").cloneNode(true);
+        let codeBlock = pageHtml.querySelector("#code");
+    
+        codeBlock.parentNode.removeChild(codeBlock);
+
+        document.querySelector("#code").appendChild(pageHtml);
+    }
+
+    main();
 }
 
 window.addEventListener("load", () => init());
