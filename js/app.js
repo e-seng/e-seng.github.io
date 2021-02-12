c2a953e3e1a3a177b130c115e964200d59ce368c
e-seng <eth.sengsavang@gmail.com>
2021-02-12 00:32:04 -0700

    Highlight syntax of copied html

--- js/app.js
+++ js/app.js
@@ -22,13 +22,64 @@ function init(){
             
             let codeLine = document.createElement("pre");
             codeLine.setAttribute("style", `text-indent: ${indent}rem`);
-            codeLine.innerText = codePart;
+            codeLine.innerHTML = highlightSyntax(codePart);
 
             document.querySelector("#code").appendChild(codeLine);
         });
     }
 
+    var syntaxFlags = {
+        "escapeChars" : {
+            "pattern" : /[\/<>&"]/g,
+            "replaceFunc" : function(flag, tag){
+                let eqChar = {
+                    "/" : "\/",
+                    "<" : "&lt;",
+                    ">" : "&gt;",
+                    "&" : "&amp;",
+                    "\"" : "&quot;"
+                }
+
+                return eqChar[tag];
+            }
+        },
+        "tag" : {
+            "pattern" : /[</]\w*[> ]/g,
+            "replaceFunc" : function(flag, target){
+                let targetItem = target.slice(1, -1);
+                let finalTag = target.slice(0, 1) + 
+                    `<span style="color: var(--html-${flag})">${targetItem}</span>`+
+                    target.slice(-1);
+
+                console.log(finalTag);
+                return finalTag;
+            }
+        },
+    }
+
+    function highlightSyntax(htmlLine){
+        let finalLine = htmlLine;
+        let count = 0;
+        // Check for html tags
+        Object.keys(syntaxFlags).forEach(function(flag){
+            if(!syntaxFlags[flag].pattern.test(finalLine)){return;}
+            finalLine = finalLine.replaceAll(
+                syntaxFlags[flag].pattern,
+                function(target){
+                    return syntaxFlags[flag].replaceFunc(target, flag);
+                }
+            );
+            console.log(Object.keys(syntaxFlags));
+        });
+
+        return finalLine;
+    }
+    
+    window["highlightSyntax"] = highlightSyntax;
+
     main();
 }
 
-window.addEventListener("load", () => init());
+window.addEventListener("load", function(){
+    init();
+});
