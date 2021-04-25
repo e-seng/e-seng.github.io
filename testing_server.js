1b2797c771d1187c7646e45dc0ac2b99cc23ecaa
Ethan Sengsavang <eth.sengsavang@gmail.com>
2021-04-25 01:55:57 -0600

    Update how server code logging is handled

--- testing_server.js
+++ testing_server.js
@@ -10,14 +10,14 @@ function error404(response){
     response.writeHead(404, {"Content-Type" : "text/plain"});
     response.write("Error 404, page not found (· £ ·) whoops");
     response.end();
-    return;
+    return 404;
 }
 
 function error500(response){
     response.writeHead(500, {"Content-Type" : "text/plain"});
     response.write("Error 500, something went wrong on our end");
     response.end();
-    return;
+    return 500;
 }
 
 function getExt(linkName){
@@ -31,22 +31,20 @@ function getHtml(request, response){
     if(request.url == "/"){filepath = path.join(ROOT, "index.html");}
 
     if(!fs.existsSync(filepath)){
-        error404(response);
-        return;
+        return error404(response);
     }
 
     try{
         data = fs.readFileSync(filepath);
     }catch(err){
-        error500(response);
         console.log(`Error: ${err}`)
-        return;
+        return error500(response);
     }
     
     response.writeHead(200, {"Content-Type" : "text/html"});
     response.write(data);
     response.end();
-    return;
+    return 200;
 }
 
 function getCss(request, response){
@@ -54,22 +52,20 @@ function getCss(request, response){
 
     filepath = path.join(ROOT, request.url.substring(1));
     if(!fs.existsSync(filepath)){
-        error404(response);
-        return;
+        return error404(response);
     }
 
     try{
         data = fs.readFileSync(filepath);
     }catch(err){
-        error500(response);
         console.log(`Error: ${err}`)
-        return;
+        return error500(response);
     }
 
     response.writeHead(200, {"Content-Type" : "text/css"});
     response.write(data);
     response.end();
-    return;
+    return 200;
 }
 
 function getJs(request, response){
@@ -79,15 +75,14 @@ function getJs(request, response){
     try{
         data = fs.readFileSync(filepath);
     }catch(err){
-        error500(response);
         console.log(`Error: ${err}`);
-        return;
+        return error500(response);
     }
 
     response.writeHead(200, {"Content-Type" : "application/javascript"});
     response.write(data);
     response.end();
-    return;
+    return 200;
 }
 
 function getPhotos(request, response){
@@ -104,15 +99,14 @@ function getPhotos(request, response){
     try{
         data = fs.readFileSync(filepath);
     }catch(err){
-        error500(response);
         console.log(`Error: ${err}`);
-        return;
+        return error500(response);
     }
 
     response.writeHead(200, {"Content-Type" : contentType});
     response.write(data);
     response.end();
-    return;
+    return 200;
 }
 
 function createLogLine(request, code){
@@ -129,19 +123,18 @@ function createLogLine(request, code){
 function onRequest(request, response){
     let photoExts = ["png", "jpeg", "jpg", "gif", "svg", "ico"]; 
 
-    let code = 200;
+    let code;
 	
     if(request.method === "GET" && (request.url === "/" || getExt(request.url) === "html")){
-        getHtml(request, response);
+        code = getHtml(request, response);
     }else if(request.method === "GET" && getExt(request.url) === "css"){
-        getCss(request, response);
+        code = getCss(request, response);
     }else if(request.method === "GET" && photoExts.includes(getExt(request.url))){
-        getPhotos(request, response);
+        code = getPhotos(request, response);
     }else if(request.method === "GET" && getExt(request.url) === "js"){
-        getJs(request, response);
+        code = getJs(request, response);
     }else{
-        error404(response);
-        code = 400
+        code = error404(response);
     }
 
 	console.log(createLogLine(request, code));
