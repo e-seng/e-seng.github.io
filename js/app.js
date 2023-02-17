64af05fcd1f5fb56fbcef6daee5d1d1c0429068b
e-seng <eth.sengsavang@gmail.com>
2023-02-17 16:11:01 -0700

    Adjust landing page to show rotating processor on load

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-function f(){
+window.addEventListener("load", () => {
+  function showLinks(){
+      var links = document.querySelector(".hidden");
+      links.classList.remove("hidden");
+  }
 
-    function showLinks(){
-        var links = document.querySelector("#links");
-        links.classList.remove("hidden");
-    }
-
-    // main
-    showLinks();
-}
-
-window.addEventListener("load", () => f());
+  // main
+  showLinks();
+});
